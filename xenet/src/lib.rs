3cd1b7a45903ed60d887047a079e857b708e836d
shellrow <81893184+shellrow@users.noreply.github.com>
2023-11-19 17:29:32 +0900

    Remove unnecessary test

--- xenet/src/lib.rs
+++ xenet/src/lib.rs
@@ -26,27 +26,3 @@ pub mod util {
         pub use xenet_packet_builder::*;
     }
 }
-
-pub fn is_supported_os() -> bool {
-    cfg!(target_os = "linux")
-        || cfg!(target_os = "windows")
-        || cfg!(target_os = "macos")
-        || cfg!(target_os = "android")
-        || cfg!(target_os = "freebsd")
-        || cfg!(target_os = "fuchsia")
-        || cfg!(target_os = "netbsd")
-        || (cfg!(target_os = "redox") && cfg!(nightly))
-        || (cfg!(target_os = "solaris") && !cfg!(target_vendor = "apple"))
-        || cfg!(target_os = "ios")
-        || cfg!(target_os = "illumos")
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_is_supported_os() {
-        assert!(is_supported_os());
-    }
-}
