909aad75745e366bac421c85ab70c77f5b894eb5
shellrow <81893184+shellrow@users.noreply.github.com>
2023-12-03 13:50:51 +0900

    Add test

--- deps/xenet-npcap-helper/src/lib.rs
+++ deps/xenet-npcap-helper/src/lib.rs
@@ -7,3 +7,25 @@ pub use self::unix::*;
 mod windows;
 #[cfg(target_os = "windows")]
 pub use self::windows::*;
+
+#[cfg(target_os = "windows")]
+#[cfg(test)]
+mod tests {
+    use crate::npcap;
+    #[test]
+    fn test_check_npcap() {
+        if npcap::npcap_installed() {
+            println!("npcap installed");
+        } else {
+            println!("npcap not installed");
+        }
+    }
+    #[test]
+    fn test_check_npcap_sdk() {
+        if npcap::npcap_sdk_installed() {
+            println!("npcap SDK installed");
+        } else {
+            println!("npcap SDK not installed");
+        }
+    }
+}
