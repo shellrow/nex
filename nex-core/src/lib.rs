4bb00afb739ea2189a5ffe1f5b817e2e8ba9905d
shellrow <81893184+shellrow@users.noreply.github.com>
2024-05-29 17:29:45 +0900

    Fix netdev compatibility

--- nex-core/src/lib.rs
+++ nex-core/src/lib.rs
@@ -1,6 +1,8 @@
 //! Provides core network types and functionality.
 //! Primarily designed for use with nex, it also includes extensions to the standard net module.
 
+pub use netdev;
+
 pub mod device;
 pub mod gateway;
 pub mod interface;
